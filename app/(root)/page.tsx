d7e7fbefc8fe4c1cd6a81dbbd3af0215ef85c17b
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-27 22:00:26 +0530

    ok

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,8 +14,6 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
-
-          <TotalBalanceBox />
         </header>
       </div>
     </section>

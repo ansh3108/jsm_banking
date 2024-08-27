b855447107d52bb62e1087dd9f7fd67a7456f9cd
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-27 22:01:47 +0530

    okk

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -6,6 +6,9 @@ const Headerbox = ({
   subtext,
   user,
 }: HeaderBoxProps) => {
+  // Add this console.log to debug the props
+  console.log({ type, title, subtext, user });
+
   return (
     <div className="header-box">
       <h1 className="header-box-title">

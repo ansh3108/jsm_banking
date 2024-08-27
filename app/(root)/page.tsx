637742bfdcdccdfad7de5faa481adef5a5dd146e
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-27 17:29:06 +0530

    shit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <section className="home">Home</section>;
+  return (
+    <section className="home">
+      <div></div>
+    </section>
+  );
 };
 
 export default Home;

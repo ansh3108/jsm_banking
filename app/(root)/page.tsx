1dc61e01093c5e46bbf0633641b05677852108ee
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-27 17:55:42 +0530

    clueless

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,14 @@
+import Headerbox from "@/components/Headerbox";
 import React from "react";
 
 const Home = () => {
   return (
     <section className="home">
       <div className="home-content">
-        <header className="home-header">Welcome, (idk who you are)</header>
+        <header className="home-header">
+          <Headerbox>
+
+        </header>
       </div>
     </section>
   );
---
+++ components/HeaderBox.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Headerbox = () => {
+  return <div>Headerbox</div>;
+};
+
+export default Headerbox;

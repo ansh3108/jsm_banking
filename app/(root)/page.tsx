8eaaca3dd2e43af5671a9fdcfd540a0303d993cc
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-29 08:56:40 +0530

    added the balance box

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -14,6 +14,12 @@ const Home = () => {
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
           />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
         </header>
       </div>
     </section>

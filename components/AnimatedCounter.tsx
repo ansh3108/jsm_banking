9e73d45001c87642b93b2bd7ba4ca7fe0dc9ee4e
Ansh Kumar <anshkumar9686@gmail.com>
2024-08-29 18:05:19 +0530

    xd

--- components/AnimatedCounter.tsx
+++ components/AnimatedCounter.tsx
@@ -4,9 +4,8 @@ import CountUp from 'react-countup'
 
 const AnimatedCounter = ({ amount } : { amount: number}) => {
   return (
-    <div>
+    <div className="w-full">
         $<CountUp
-        duration={2.75}
         decimals={2}
         decimal=','
         prefix='$'

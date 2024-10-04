c2ebc758d83ba721f207b1f43a5e3782cdda60ed
Ansh Kumar <anshkumar9686@gmail.com>
2024-10-04 23:33:59 +0530

    error fixes

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -11,7 +11,7 @@ const Headerbox = ({
       <h1 className="header-box-title">
         {title}
         {type === "greeting" && (
-          <span className="text-bankGradient">&nbsp;{user}</span>2
+          <span className="text-bankGradient">&nbsp;{user}</span>
         )}
       </h1>
       <p className="header-box-subtext">{subtext}</p>
